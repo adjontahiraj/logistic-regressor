311af4575b4b81f96dd87c2ac25575c462e86bc9
Adjon Tahiraj <atahiraj@umail.ucsb.edu>
2020-02-09 18:57:50 -0800

    starting

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import numpy as np
+import math
+import random
 from sgd import SGDSolver
 
 def load_data(filename):
@@ -9,11 +11,17 @@ def load_data(filename):
             y values (numpy array: n x 1)
     """
     data = np.load(filename)
+    #print(data)
+    data_x = np.zeros((len(data), 11))
+    data_y = np.zeros(len(data))
     for i in range(len(data)):
-        print(data[i])
-        print("\n")
-    data_x, data_y = []
-    # TODO: Finish this function here.
+        data_x[i] = data[i][0:11]
+        if(data[i][-1] <= 4):
+            data_y[i] = 0
+        elif(data[i][-1] <= 6):
+            data_y[i] = 1
+        else:
+            data_y[i] = 2
     return data_x, data_y
 
 def red_wine_run(train_red_x, train_red_y, test_red_x, test_red_y):
@@ -21,22 +29,27 @@ def red_wine_run(train_red_x, train_red_y, test_red_x, test_red_y):
     print('---------------\nRed Wine Data\n---------------')
 
     # Training Phase
-    # optimal values from 2D-grid search
-    lam     = 0.0       # regularization weight
-    alpha   = 0.0       # learning rate
-    nepochs = 10        # sample # of epochs
+    # values for 2D-grid search
+    lam     = []        # regularization weight [min, max]
+    alpha   = []        # learning rate [min, max]
+    nepochs = []        # sample # of epochs
     epsilon = 0.0       # epsilon value
     param   = []
+    # end TODO
 
     # using this alpha and lambda values run the training
     print(f"alpha: {alpha}, lambda:{lam}")
     print("Running Training phase")
-    param = SGDSolver('Training', train_red_x, train_red_y, alpha, lam, nepochs, epsilon, param)
+    # return param and optimal values for alpha and lambda from SGDSolver
+    param, alpha, lam = SGDSolver('Training', train_red_x, train_red_y, alpha, lam, nepochs, epsilon, param)
 
+    # optimal values from 2-D search
+    print(f"optimal values\nalpha: {alpha}, lambda: {lam}")
 
+    # Note: validation and testing phases only take a single value for (alpha, lam) and not a list. 
     # Validation Phase
-    x_mse_val = SGDSolver('Validation', test_red_x, test_red_y, alpha, lam, nepochs, epsilon, param)
-    print(f"Current Red Wine Data MSE is: {mse_val}.")
+    x_mse = SGDSolver('Validation', test_red_x, test_red_y, alpha, lam, nepochs, epsilon, param)
+    print(f"Current Red Wine Data MSE is: {x_mse}.")
 
     # Testing Phase
     red_wine_predicted = SGDSolver('Testing', test_red_x, test_red_y, alpha, lam, nepochs, epsilon, param)
@@ -44,21 +57,33 @@ def red_wine_run(train_red_x, train_red_y, test_red_x, test_red_y):
     for i in range(0, 50):
         print(f"Predicted: {red_wine_predicted[i]}, Real: {test_red_y[i]}")
 
-def white_wine_run(train_white_x, train_white_y, test_white_x. test_white_y):
+def white_wine_run(train_white_x, train_white_y, test_white_x, test_white_y):
     # White wine data
     print('---------------\nWhite Wine Data\n---------------')
 
     # TODO: Change hyperparameter values here as needed 
+    # similar to red_wine_run
+    # values for 2D-grid search
+    lam     = []        # regularization weight [min, max]
+    alpha   = []        # learning rate [min, max]
+    nepochs = []        # sample # of epochs
+    epsilon = 0.0       # epsilon value
+    param   = []
+    # end TODO
 
     # Training Phase
     print(f"alpha: {alpha}, lambda:{lam}")
     print("Running Training phase")
-    param = SGDSolver('Training', train_white_x, train_white_y, alpha, lam, nepochs, epsilon, param)
+    # return param and optimal values for alpha and lambda from SGDSolver
+    param, alpha, lam = SGDSolver('Training', train_white_x, train_white_y, alpha, lam, nepochs, epsilon, param)
 
+    # optimal values from 2-D search
+    print(f"optimal values\nalpha: {alpha}, lambda: {lam}")
 
+    # Note: validation and testing phases only take a single value for (alpha, lam) and not a list. 
     # Validation Phase
-    x_mse_val = SGDSolver('Validation', test_white_x, test_white_y, alpha, lam, nepochs, epsilon, param)
-    print(f"Current White Wine Data MSE is: {mse_val}.")
+    x_mse = SGDSolver('Validation', test_white_x, test_white_y, alpha, lam, nepochs, epsilon, param)
+    print(f"Current White Wine Data MSE is: {x_mse}.")
 
     # Testing Phase
     white_wine_predicted = SGDSolver('Testing', test_white_x, test_white_y, alpha, lam, nepochs, epsilon, param)
@@ -69,16 +94,14 @@ def white_wine_run(train_white_x, train_white_y, test_white_x. test_white_y):
 def main():
     # import all the data
     # TODO: call the load_data() function here and load data from file
-    train_red_x, train_red_y    = load_data('hw2_winequality-red_train.npy')
-    test_red_x, test_red_y      = load_data('hw2_winequality-red_test.npy')
+    train_red_x, train_red_y        = load_data('hw2_winequality-red_train.npy')
+    test_red_x, test_red_y          = load_data('hw2_winequality-red_test.npy')
     train_white_x, train_white_y    = load_data('hw2_winequality-white_train.npy')
     test_white_x, test_white_y      = load_data('hw2_winequality-white_test.npy')
     
     # Tests
-    red_wine_run(train_red_x, train_red_y, test_red_x. test_red_y)
-    white_wine_run(train_white_x, train_white_y, test_white_x. test_white_y)
+    red_wine_run(train_red_x, train_red_y, test_red_x, test_red_y)
+    white_wine_run(train_white_x, train_white_y, test_white_x, test_white_y)
 
 if __name__ == "__main__":
     main()
-
-output = load_data('hw2_winequality-red_train.npy')
